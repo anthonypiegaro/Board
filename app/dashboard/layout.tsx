1de7fad6eb8a9cab80f17077b2b14ea13621f780
Anthony Piegaro <84252391+anthonypiegaro@users.noreply.github.com>
2025-08-22 23:15:56 +0000

    Add Signout Button

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,6 +4,7 @@ import { SquareKanban } from "lucide-react"
 import { ThemeTrigger } from "@/components/theme-trigger"
 
 import { Breadcrumb } from "./breadrcumb"
+import { ProfileButton } from "./profile-button"
 
 export default function Layout({
   children
@@ -13,7 +14,7 @@ export default function Layout({
   return (
     <div className="w-svw h-svh p-2 bg-sidebar">
       <div className="flex bg-background rounded-md flex-col w-full h-full overflow:hidden">
-        <div className="w-full p-2 flex gap-x-4 border-b-2 dark:border-neutral-600 items-center">
+        <div className="w-full md:px-4 p-2 flex gap-x-4 border-b-2 dark:border-neutral-600 items-center">
           <Link 
             className="flex items-center gap-x-1 font-semibold text-lg"
             href="/dashboard"
@@ -23,7 +24,10 @@ export default function Layout({
           </Link>
           <div className="h-5 w-[2px] bg-neutral-300 dark:bg-neutral-600 rounded-sm" />
           <Breadcrumb />
-          <ThemeTrigger className="ml-auto hover:bg-fuchsia-200/60 dark:hover:bg-fuchsia-400/60" size="size-6"/>
+          <div className="flex gap-x-1 ml-auto">
+            <ThemeTrigger className="hover:bg-fuchsia-200/60 dark:hover:bg-fuchsia-400/60" size="size-6"/>
+            <ProfileButton />
+          </div>
         </div>
         <div className="grow overflow-auto">
           {children}
---
+++ app/dashboard/profile-button.tsx
@@ -0,0 +1,47 @@
+
+"use client"
+
+import { useRouter } from "next/navigation"
+import { User } from "lucide-react"
+
+import { 
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem
+} from "@/components/ui/dropdown-menu"
+import { authClient } from "@/lib/auth-client"
+
+export function ProfileButton() {
+  const router = useRouter()
+
+  const handleSignOut = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+        },
+      },
+    });
+  }
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div
+          role="button"
+          className="text-foreground/80 dark:text-foreground relative flex items-center justify-center transition-all duration-300 rounded-md hover:bg-fuchsia-200/60 dark:hover:bg-fuchsia-400/60 p-1"
+        >
+          <User className="w-7 h-7" />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem 
+          variant="destructive"
+          onClick={handleSignOut}
+        >
+          Sign out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
